6e758cbbec7ae0b69b00009a9cf8e891e6b67cbe
Zăn Zăn <69894094+Van-IoT2020@users.noreply.github.com>
2020-12-23 14:16:45 +0700

    view to get data when search by vietnamese

--- modules/product/content/product_name_search.php
+++ modules/product/content/product_name_search.php
@@ -19,3 +19,65 @@
         return $str;
     }
 ?>
+
+<ul class="aa-product-catg">
+    <?php
+    $name = create_slug($_GET['search']);
+
+    function getDataProductListnotype($conn,$name,$start,$onePerPage) {
+        $sm = $conn->prepare("SELECT * FROM product WHERE name like N'%$name%' ORDER BY id DESC LIMIT $start, $onePerPage");
+        $sm->execute();
+        $data = $sm->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }
+
+
+    function rowTotal($conn,$name) {
+        $sm = $conn->prepare("SELECT * FROM product WHERE name like N'%$name%'");
+        $sm->execute();
+        return $sm->rowCount();
+    }
+        
+        $totalRow = rowTotal($conn,$name);
+        $totalPage = ceil($totalRow/$onePerPage);
+
+        if(isset($_GET['page'])) {
+            $page = $_GET['page'];
+            if($page > $totalPage || $page < 1) {
+                $page = 1;
+            }
+        } else {
+            $page = 1;
+        }
+        $start = ($page - 1) * $onePerPage;
+        $data = getDataProductListnotype($conn,$name,$start,$onePerPage);
+        
+
+        foreach($data as $item) {
+        ?>
+    <!-- start single product item -->
+    <li>
+        <figure>
+            <a class="aa-product-img" href="index.php?p=detail&id=<?php echo $item['id']; ?>"><img width="100%"
+                    height="100%" src="public/image/product/<?php echo $item['avatar'];?>"
+                    alt="Xem chi tiết sản phẩm"></a>
+            <a class="aa-add-card-btn" href="index.php?p=buynow&idsp=<?php echo $item["id"] ?>">Mua ngay</a>
+            <figcaption>
+                <h4 class="aa-product-title"><a href="#"><?php echo $item['name']?></a></h4>
+                <span class="aa-product-price"><?php echo number_format($item["price"]); ?>VNĐ</span><span
+                    class="aa-product-price"><del
+                        style="font-size:13px;"><?php echo number_format($item["price"]*1.5); ?>VNĐ</del></span>
+            </figcaption>
+        </figure>
+
+        <div class="aa-product-hvr-content">
+            <a href="index.php?p=product&idsp=<?php echo $item['id']; ?>" data-toggle="tooltip" data-placement="top"
+                title="Thêm vào giỏ hàng"><span class="fa fa-shopping-cart fa-3x"></span></a>
+        </div>
+        <!-- product badge -->
+        <span class="aa-badge aa-sale" href="#">SALE!</span>
+    </li>
+    <?php
+        }
+?>
+</ul>
