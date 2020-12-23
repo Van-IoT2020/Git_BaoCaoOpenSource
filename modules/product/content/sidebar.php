2f6eb1132a26b5176a1ef45279d45f9b3d6bbe8b
Viet Hoang <hoanggohoanggocm10101@gmail.com>
2020-12-23 14:40:08 +0700

    get data to show sidebar

--- modules/product/content/sidebar.php
+++ modules/product/content/sidebar.php
@@ -21,3 +21,105 @@
         return $data;
     }
 ?>
+<aside class="aa-sidebar">
+    <!-- single sidebar -->
+    <div class="aa-sidebar-widget">
+        <h3>Loại sản phẩm</h3>
+        <ul class="aa-catg-nav">
+            <?php
+            include "admin/model/producttype.php";
+            $data = getDataProducttypeList($conn);
+            foreach ($data as $item) {
+            ?>
+                <li><a href="index.php?p=product&type=<?php echo $item['id'] ?>"><?php echo $item['name'] ?></a></li>
+            <?php
+            }
+            ?>
+        </ul>
+    </div>
+
+    <!-- single sidebar -->
+    <div class="aa-sidebar-widget">
+        <h3>Những sản phẩm xem gần đây</h3>
+        <div class="aa-recently-views">
+            <ul>
+                <?php
+                    
+
+                    // Check to see if there is a session
+                    if(empty($_SESSION['JustFinishedWatching'])){
+                        //create $_SESSION['JustFinishedWatching']
+                        $_SESSION['JustFinishedWatching'] = array();
+                        //Get 4 products from the database
+                        $datafourproduct = fourProduct($conn);
+                        // Add these 4 products to the session
+                        foreach($datafourproduct as $item) {
+                            $_SESSION['JustFinishedWatching'][] = $item['id'];
+                        }
+                        if(!empty($_SESSION['JustFinishedWatching'])) {
+                            $dataWatching = showFourProduct($conn,$_SESSION['JustFinishedWatching']);
+                            foreach($dataWatching as $item) {
+                        ?>
+                        <li>
+                            <a href="index.php?p=detail&id=<?php echo $item['id']; ?>" class="aa-cartbox-img"><img alt="img" src="public/image/product/<?php echo $item['avatar']; ?>"></a>
+                            <div class="aa-cartbox-info">
+                                <h4><a href="index.php?p=detail&id=<?php echo $item['id']; ?>"><?php echo $item['name']; ?></a></h4>
+                                <p><?php echo number_format($item["price"]); ?>VNĐ</p>
+                                <p><del style="font-size:13px;"><?php echo number_format($item["price"]*1.5); ?>VNĐ</del></p>
+                            </div>
+                        </li>
+                        <?php
+                            }
+                        }
+
+
+                    } else {
+                        // had $_SESSION['JustFinishedWatching']
+                        if(!empty($_SESSION['JustFinishedWatching'])) {
+                            $dataWatching = showFourProduct($conn,$_SESSION['JustFinishedWatching']);
+                            foreach($dataWatching as $item) {
+                        ?>
+                        <li>
+                            <a href="index.php?p=detail&id=<?php echo $item['id']; ?>" class="aa-cartbox-img"><img alt="img" src="public/image/product/<?php echo $item['avatar']; ?>"></a>
+                            <div class="aa-cartbox-info">
+                                <h4><a href="index.php?p=detail&id=<?php echo $item['id']; ?>"><?php echo $item['name']; ?></a></h4>
+                                <p><?php //echo number_format($item["price"]); ?>VNĐ</p>
+                                <!-- BUG IS HERE -->
+                                <p><?php echo number_format($item["pricew"]); ?>VNĐ</p>
+                                <p><del style="font-size:13px;"><?php echo number_format($item["price"]*1.5); ?>VNĐ</del></p>
+                            </div>
+                        </li>
+                        <?php
+                            }
+                        }
+                    }
+                ?>
+
+            </ul>
+        </div>
+    </div>
+    <!-- single sidebar -->
+    <div class="aa-sidebar-widget">
+        <h3>Những sản phẩm mới nhất</h3>
+        <div class="aa-recently-views">
+            <ul>
+                <?php 
+                    $dataNew = fourProduct($conn);
+                    foreach($dataNew as $item) {
+                ?>
+                <li>
+                <a href="index.php?p=detail&id=<?php echo $item['id']; ?>" class="aa-cartbox-img"><img alt="img" src="public/image/product/<?php echo $item['avatar']; ?>"></a>
+                    <div class="aa-cartbox-info">
+                        <h4><a href="index.php?p=detail&id=<?php echo $item['id']; ?>"><?php echo $item['name']; ?></a></h4>
+                        <p><?php echo number_format($item["price"]); ?>VNĐ</p>
+                        <p><del style="font-size:13px;"><?php echo number_format($item["price"]*1.5); ?>VNĐ</del></p>
+                    </div>
+                </li>
+                <?php
+                    } 
+                ?>
+
+            </ul>
+        </div>
+    </div>
+</aside>
